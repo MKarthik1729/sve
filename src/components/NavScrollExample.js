a511712f934a0b829af4c820e2b29d4a8612034e
crazycoder1729 <crazycoder1729@gmail.com>
2023-12-12 10:44:06 +0530

    navbar

--- src/components/NavScrollExample.js
+++ src/components/NavScrollExample.js
@@ -8,32 +8,30 @@ function NavScrollExample() {
   const param1 = useLocation().pathname
   console.log(param1)
   const highlight = {
-    // backgroundColor:"aliceblue",
     color : '#f37e00',
-    // padding: '20px 20px'
-    margin: '0 20px' 
+    margin: '0 30px' 
   }
   const Gapsetting = { margin: '0 30px' }
   return (
-    <Navbar expand="lg" className="bg-body-tertiary" style={{margin:"0"}}>
+    <Navbar expand="lg" style={{padding:"0",margin:"0"}} >
       <Container fluid>
-        <Navbar.Brand href="/" style={{height:"80px",overflow:"hidden" }} ><Logo /></Navbar.Brand>
+        <Navbar.Brand href="/" style={{height:"80px",overflow:"hidden",margin:"0",padding:"0"}} ><Logo /></Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
             className="me-auto my-2 my-lg-0"
             // style={{ maxHeight: '100px' }}
             style={{padding:"1% 1%"}}
-            navbarScroll
+            // navbarScroll
           >
             </Nav>
-<Form className="d-flex">
+{/* <Form className="d-flex"> */}
   <Nav.Link href="/" style={(param1==="/")?highlight:Gapsetting}>HOME</Nav.Link>
   <Nav.Link href="/about"  style={(param1==="/about")?highlight:Gapsetting}>ABOUT US</Nav.Link>
   <Nav.Link href="/machining" style={(param1==="/machining")?highlight:Gapsetting}>BUSINESS VERTICALS</Nav.Link>
   {/* <Nav.Link href="/our-team" style={(param1==="/our-team")?highlight:Gapsetting}>TEAM SVE</Nav.Link> */}
   <Nav.Link href="/contact-us" style={(param1==="/contact-us")?highlight:Gapsetting}>CONTACT US</Nav.Link>
-</Form>
+{/* </Form> */}
 
 
         </Navbar.Collapse>
