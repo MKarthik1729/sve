a57e2dde4fc3a1e32313dc016d17ce02f52cc6e4
crazycoder1729 <crazycoder1729@gmail.com>
2023-12-12 15:52:12 +0530

    padding to home page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,7 +7,7 @@ function Contact() {
         <p>We are happy and glad to establish contacts with you, please write to Gnanavel@sriveluenterprises.com</p>
         <p>for general queries and get in touch with any of Succinnovian listed below for specific queries or swift responses.</p>
         <div >
-          <img src={'/velu.jpeg'} style={{ height: "250px" }} />
+          <img src={'/velu.jpeg'} style={{ height: "250px" }} alt='searching...'/>
           <p>Gnanavelu s, Director, +91 9980066665, Gnanavel@sriveluenterprises.com</p>
         </div><br />
         <p>Sri Velu Enterprises operates in IST time zone, you will get a response within 24 hours the maximum <br />if you happen
