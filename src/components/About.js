3c41c739e8cf296f3fd6d18f89e0ada4a0fabcd3
crazycoder1729 <crazycoder1729@gmail.com>
2023-12-21 17:09:58 +0530

    about us debug

--- src/components/About.js
+++ src/components/About.js
@@ -8,42 +8,42 @@ import EngineeringIcon from '@mui/icons-material/Engineering';
 import RuleIcon from '@mui/icons-material/Rule';
 function About() {
   const IconStyle = {
-    fontSize:"100px",
+    fontSize:"120px",
     backgroundColor:"white",
     padding:"10px",
     color:"red",
     borderRadius:"50%"
   }
   return (
-    <div>
+    <div className='center'>
       <div className="b2  con">
       <div className="column">
         <div className="centered-text">
-          <h2 style={{fontWeight:"200",fontSize:"50px"}}>About Us</h2>
+          <h2 style={{fontSize:"50px"}}>About Us</h2>
         </div>
       </div>
 
       <div className="column">
-        <div className="centered-text" style={{padding:"70px"}}>
+        <div className="centered-text pad">
           <p style={{fontSize:'20px'}}>Sri Velu Enterprises is a privately held; AS9100 Rev D and ISO 9001 – 2015 certified manufacturer of precision machined parts and service provider in the field of Global supply chain management. The original vision of the founders, which continues to this day, is to deliver components and assemblies machined to the highest standards and services to be of outstanding value for our clients.</p>
         </div>
       </div>
     </div>
 
     <div className="b1  con">
+    <div className="column">
+        <div className="centered-text">
+          <h2 style={{fontSize:"50px"}}>Why Us</h2>
+        </div> 
+      </div>
       <div className="column">
-        <ul className="centered-text" style={{padding:"50px"}}>
+        <ul className="centered-text">
           <li style={{fontSize:'20px'}}>Visionary organization driven by Values and Culture.</li>
           <li style={{fontSize:'20px'}}>Nurture continues improvements to sustain and graduate relationships by Technology upgrades, VAVE, Productivity improvements.</li>
           <li style={{fontSize:'20px'}}>Willing to extend supply chain support on TCO and mitigate risk through VMI, Kanban, Consignment.</li>
           <li style={{fontSize:'20px'}}>Flexible and efficient in handling High mix low volume parts providing solutions to every part.</li>
           <li style={{fontSize:'20px'}}>Market oriented to support customers in both Spike and Dip</li>
         </ul>
-      </div>
-    <div className="column">
-        <div className="centered-text">
-          <h2 style={{fontWeight:"200",fontSize:"50px"}}>Why Us</h2>
-        </div> 
       </div>
 
     </div>
