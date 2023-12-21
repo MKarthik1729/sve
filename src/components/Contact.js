fae85fa1832db4508d6e5899fd7cf956dc651870
crazycoder1729 <crazycoder1729@gmail.com>
2023-12-21 17:44:20 +0530

    address middle

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -13,28 +13,30 @@ function Contact() {
         <p>Sri Velu Enterprises operates in IST time zone, you will get a response within 24 hours the maximum <br />if you happen
           to be from the time zone with a time difference of 6+ hours to IST.</p>
       </div>
-      <div className='contact'>
+      <div className='contact' >
+      <div  style={{display:"flex",justifyContent:"center",alignItems:"center"}} >
+
       <div>
         <h2>Contact us</h2>
-      Sri Velu Enterprises<br />
+     <p> Sri Velu Enterprises<br />
       Contact Person : Gnanavel S.(Proprietor)<br /><br />
       N0.11/1,Chamundi Vihar, Lakshmipura Road, <br />
       Abbigere, Chikkabanavara Post, Bengaluru 5600090, <br />
       Karnataka, India<br /><br />
       Mobile : +91 9980066665<br />
-      EMAIL ID : gnanavel@sriveluenterprises.com<br />
+      EMAIL ID : gnanavel@sriveluenterprises.com</p >
+        </div>
       </div>
+      <div className='center'>
+
       <iframe 
-      src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15545.54306060589!2d77.5289043!3d13.0747205!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae22c30d630cd9%3A0x70a2435e865285cc!2sSri%20Velu%20Enterprises!5e0!3m2!1sen!2sin!4v1703151248837!5m2!1sen!2sin" 
-      // width="30%" 
+      src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15545.54306060589!2d77.5289043!3d13.0747205!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae22c30d630cd9%3A0x70a2435e865285cc!2sSri%20Velu%20Enterprises!5e0!3m2!1sen!2sin!4v1703151248837!5m2!1sen!2sin"  
       height="450" 
       // style={{border:'0'}}
-      style={{padding:'15px 0'}}
+      style={{alignSelf:'center',justifySelf:"center"}}
       loading="lazy" 
       referrerpolicy="no-referrer-when-downgrade" />
-      {/* <div className='column'>
-        Query
-      </div> */}ṣ
+      </div>
       </div>
       </div>
   )
